877f1b636690daa21a03724098636c3b060ac3ea
Gorkavik Tatiana <tatiana.gorkanik@exposit.com>
2020-03-25 17:25:30 +0300

    feat: add solutions for tasks

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,6 +4,15 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var inputArray = input.toMutableList()
+        var outputArray = mutableListOf<Int>()
+        var min = inputArray.min()!!
+        var max = inputArray.max()!!
+        var sum = inputArray.sum()
+
+        outputArray.add(sum-max)
+        outputArray.add(sum-min)
+
+        return  outputArray.toIntArray()
     }
 }
--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -2,9 +2,16 @@ package subtask3
 
 class BillCounter {
 
+    private val message = "bon appetit"
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        val result = (bill.sum() - bill[k])/2
+
+        if(result == b){
+            return message
+        }else{
+            return (b - result).toString()
+        }
     }
 }
